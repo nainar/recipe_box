75d6b779c3cffa99e14bacbfe6a7d0105f8360c7
Naina Raisinghani <nainar@chromium.org>
2016-01-17 21:06:34 +1100

    getting images working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  Paperclip.options[:command_path] = "/usr/local/bin/"
+  Paperclip.options[:command_path] = "/usr/local/bin/identify"
+  Paperclip.options[:log] = true
+   Paperclip.options[:log_command] = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
